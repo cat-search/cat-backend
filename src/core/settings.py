4653202e19e0a78c794ef1c3d8bcea947cdd5d6c
Valeriy <lev.370x@gmail.com>
2025-04-18 20:26:12 +0200

    settings ollama

--- src/core/settings.py
+++ src/core/settings.py
@@ -17,7 +17,8 @@ class Settings(BaseSettings):
     vdb_type: str        = 'weaviate'
 
     # Vector DB. weaviate
-    weaviate_host: str                  = "cat-vm2.v6.rocks"
+    # weaviate_host: str                  = "localhost"
+    weaviate_host: str                  = "weaviate"
     weaviate_port: int                  = 8080
     weaviate_grpc_port: int             = 50051
     weaviate_api_key: str               = "Search_the_VK"
@@ -25,7 +26,11 @@ class Settings(BaseSettings):
     weaviate_api_endpoint: str          = "http://ollama:11434"
     # Model name. If it's `None`, uses the server-defined default
     weaviate_model: str                 = "nomic-embed-text"
-    weaviate_doc_limit: int   = 5
+    weaviate_doc_limit: int             = 5
+
+    llm_url: str                        = "http://localhost:11434"
+    llm_model: str                      = "llama3"
+    # llm_doc_limit: int                  = 5
 
 
 settings = Settings()
